554147159e7e32605af8b27cf2da3c3448da803d
Levente Krotos <krotos.levi@gmail.com>
2023-03-22 16:36:12 +0100

    deployment

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial-top': 'radial-gradient(circle at top, var(--tw-gradient-stops))',
+        'triangle': "url('/assets/images/bg-triangle.svg')",
+      }
+    },
+  },
+  plugins: [],
+}
